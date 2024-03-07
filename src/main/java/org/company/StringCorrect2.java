f700b0f59a9c1ab6490f3f1ff62b9573e4124fdb
Badman1332 <mik.davidsson@gmail.com>
2024-03-07 14:24:54 +0100

    Test strings

---
+++ src/main/java/org/company/StringCorrect2.java
@@ -0,0 +1,10 @@
+package org.company;
+
+public class StringCorrect2 {
+
+    public static void main(String[] args) {
+        String string = "\tJ\ta\tv\ta\t";
+        String trimmedString = string.trim();
+        System.out.println(trimmedString);
+    }
+}
