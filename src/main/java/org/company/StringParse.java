d1c5a0bf43ff30c9a5065120294af6b2762f4a64
Badman1332 <mik.davidsson@gmail.com>
2024-03-07 14:25:40 +0100

    Test strings

---
+++ src/main/java/org/company/StringParse.java
@@ -0,0 +1,10 @@
+package org.company;
+
+public class StringParse {
+    public static void main(String[] args) {
+        int number = 30;
+        String stringNumber = String.valueOf(number);
+        String combinedString = stringNumber + "30";
+        System.out.println(combinedString);
+    }
+}
