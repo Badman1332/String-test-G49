2cd5ba8964023683e55782f4b2760597c8fa9361
Badman1332 <mik.davidsson@gmail.com>
2024-03-07 14:25:53 +0100

    Test strings

---
+++ src/main/java/org/company/StringToChar.java
@@ -0,0 +1,10 @@
+package org.company;
+
+public class StringToChar {
+
+    public static void main(String[] args) {
+        char[] charArray = {'J', 'a', 'v', 'a'};
+        String stringFromCharArray = new String(charArray);
+        System.out.println(stringFromCharArray);
+    }
+}
